22f1d494f0ed62b7958b1b8b987e51b9f774f28a
Thora Magnusdottir <70522885+thoraaslaug@users.noreply.github.com>
2023-11-08 20:42:25 +0100

    added audio + animation that works pretty well

---
+++ Assets/PlayMultiple.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMultiple : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip audioClip1; 
+    public AudioClip audioClip2; 
+
+    public void PlayAudioClip1()
+    {
+        // Set the audio clip for the AudioSource and play it
+        audioSource.clip = audioClip1;
+        audioSource.Play();
+    }
+
+    public void PlayAudioClip2()
+    {
+        // Set the audio clip for the AudioSource and play it
+        audioSource.clip = audioClip2;
+        audioSource.Play();
+    }
+}
